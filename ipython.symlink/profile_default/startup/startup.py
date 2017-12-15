9cd2da68ab8b42da830a9b9b1c1e3679224d94ac
Tom Bugnon <tombugnon@hotmail.com>
2017-12-15 15:17:16 -0600

    Add ipython config and don't read pythonstartup.py in IP

---
+++ ipython.symlink/profile_default/startup/startup.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import re
+# from fractions import Fraction
+# from datetime import date, datetime, timedelta
+# from collections import Counter, defaultdict, namedtuple
+# from hashlib import md5, sha1
+try:
+    import itertools
+except:
+    pass
+# import json
+# import base64
+# from requests import get, head, post
+try:
+    import numpy as np
+except:
+    pass
+
+try:
+    import nest
+except:
+    pass
+
+try:
+    nest.Install('bmtneuronmodule')
+except:
+    pass
+
+try:
+    nest.Install('rotatedrectangularmaskmodule')
+except:
+    pass
