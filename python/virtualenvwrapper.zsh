9e7651080a16b463e12eb03eb12acc74fa7542ee
Tom Bugnon <tombugnon@hotmail.com>
2017-12-17 19:26:32 -0600

    Fix virtualenv init for multiple machines

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,5 +1,11 @@
 # virtualenvwrapper configuation:
-export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
+
+if [ $HOST = Dosto ]
+then
+  export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python3"
+else
+  export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
+fi
 export WORKON_HOME="$HOME/.virtualenvs"
 export PROJECT_HOME=$CODE
 source "/usr/local/bin/virtualenvwrapper.sh"
